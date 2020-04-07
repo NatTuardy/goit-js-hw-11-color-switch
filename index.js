410e5efc88023422e044cf83853f1a0d7295347e
Your Name <you@example.com>
2020-04-07 23:54:07 +0300

    added new folder

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+  body: document.body,
+};
+
+const color = {
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.colorId = setInterval(() => {
+      refs.body.style.backgroundColor =
+        colors[randomIntegerFromInterval(0, colors.length - 1)];
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.colorId);
+    this.isActive = false;
+  },
+};
+
+color.start();
+
+refs.startBtn.addEventListener('click', color.start.bind(color));
+refs.stopBtn.addEventListener('click', color.stop.bind(color));
